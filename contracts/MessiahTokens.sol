5388839d1fc7b946db89fef4ae0b4e527ebe51a9
derodero24 <k.itsh20524abc@gmail.com>
2022-04-17 16:57:04 +0900

    change license

--- contracts/MessiahTokens.sol
+++ contracts/MessiahTokens.sol
@@ -1,7 +1,8 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
