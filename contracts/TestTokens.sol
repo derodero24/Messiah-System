3d12d1a0ad7da61a551a325b3d08e89c6ee48055
derodero24 <k.itsh20524abc@gmail.com>
2022-05-06 22:34:40 +0900

    update

--- contracts/TestTokens.sol
+++ contracts/TestTokens.sol
@@ -13,6 +13,10 @@ contract SimpleERC20 is ERC20, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
 }
 
 contract SimpleERC721 is ERC721, Ownable {
