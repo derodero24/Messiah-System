e95210fde0a0a672f315ce43a70265b2dbc89a0f
esper66 <65485839+esper66@users.noreply.github.com>
2022-05-07 02:08:31 +0900

    address

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,12 +2,12 @@ import { ethers } from 'hardhat';
 
 async function main() {
   const accounts = [
-    '0xb199c3D287a207A215DF500ffA06C58F26000713',
-    '0xE3D094a5C68732C9E5D6574AC4071dC0d8bE151E',
-    '0xE3D094a5C68732C9E5D6574AC4071dC0d8bE151E',
-    '0xE3D094a5C68732C9E5D6574AC4071dC0d8bE151E',
-    '0xE3D094a5C68732C9E5D6574AC4071dC0d8bE151E',
-    '0xE3D094a5C68732C9E5D6574AC4071dC0d8bE151E',
+    '0x84f81f38Dc4022E07cd2E0a53d2300bf261cE3E4',
+    '0x6aB12FfB76594E2e74eff3FD005E167968c08513',
+    '0x9B1Eb2675936b3e34A4d348F3e578fF4Cf712a07',
+    '0xbDe3fc03A1d368ad635aa1be17fEd5E55c801Ed5',
+    '0xD7457Ff5a1600f79098f22F44297Cf616F2bFA57',
+    '0x7a45E14e948B8072956590c6b9fa58DA099F1431',
   ];
   const signers = await ethers.getSigners();
 
@@ -20,18 +20,18 @@ async function main() {
   // MessiahSystemFactory
   await ethers
     .getContractFactory('MessiahSystemFactory')
-    .then(factory => factory.deploy().then(contract => contract.deployed()))
-    .then(contract =>
+    .then((factory) => factory.deploy().then((contract) => contract.deployed()))
+    .then((contract) =>
       console.log('MessiahSystemFactory deployed to:', contract.address)
     );
 
   // Main token
   await ethers
     .getContractFactory('SimpleERC721')
-    .then(factory =>
-      factory.deploy('AsmaH', 'ASMA').then(contract => contract.deployed())
+    .then((factory) =>
+      factory.deploy('AsmaH', 'ASMA').then((contract) => contract.deployed())
     )
-    .then(token => {
+    .then((token) => {
       accounts.map((account, idx) => token.safeMint(account, idx));
       console.log('SimpleERC721 deployed to:', token.address);
     });
@@ -39,10 +39,10 @@ async function main() {
   // Sub token
   await ethers
     .getContractFactory('SimpleERC20')
-    .then(factory =>
-      factory.deploy('AstH', 'ASH').then(contract => contract.deployed())
+    .then((factory) =>
+      factory.deploy('AstH', 'ASH').then((contract) => contract.deployed())
     )
-    .then(token => {
+    .then((token) => {
       token.mint(accounts[0], 800000);
       token.mint(accounts[1], 100000);
       token.mint(accounts[2], 50000);
@@ -51,7 +51,7 @@ async function main() {
     });
 }
 
-main().catch(error => {
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
