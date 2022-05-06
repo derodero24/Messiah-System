d9ba09cc35845d58227d4f5f26c88edaac28b8d6
derodero24 <k.itsh20524abc@gmail.com>
2022-05-07 02:18:27 +0900

    fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,49 +9,51 @@ async function main() {
     '0xD7457Ff5a1600f79098f22F44297Cf616F2bFA57',
     '0x7a45E14e948B8072956590c6b9fa58DA099F1431',
   ];
-  const signers = await ethers.getSigners();
 
+  // const signers = await ethers.getSigners();
   // Send Ether
-  await signers[0].sendTransaction({
-    to: accounts[0],
-    value: ethers.utils.parseEther('1.0'),
-  });
+  // await signers[0].sendTransaction({
+  //   to: accounts[0],
+  //   value: ethers.utils.parseEther('1.0'),
+  // });
 
   // MessiahSystemFactory
   await ethers
     .getContractFactory('MessiahSystemFactory')
-    .then((factory) => factory.deploy().then((contract) => contract.deployed()))
-    .then((contract) =>
+    .then(factory => factory.deploy().then(contract => contract.deployed()))
+    .then(contract =>
       console.log('MessiahSystemFactory deployed to:', contract.address)
     );
 
   // Main token
   await ethers
     .getContractFactory('SimpleERC721')
-    .then((factory) =>
-      factory.deploy('AsmaH', 'ASMA').then((contract) => contract.deployed())
+    .then(factory =>
+      factory.deploy('AsmaH', 'ASMA').then(contract => contract.deployed())
     )
-    .then((token) => {
-      accounts.map((account, idx) => token.safeMint(account, idx));
+    .then(async token => {
+      for (let i = 0; i < accounts.length; i++) {
+        await token.safeMint(accounts[i], i);
+      }
       console.log('SimpleERC721 deployed to:', token.address);
     });
 
   // Sub token
   await ethers
     .getContractFactory('SimpleERC20')
-    .then((factory) =>
-      factory.deploy('AstH', 'ASH').then((contract) => contract.deployed())
+    .then(factory =>
+      factory.deploy('AstH', 'ASH').then(contract => contract.deployed())
     )
-    .then((token) => {
-      token.mint(accounts[0], 800000);
-      token.mint(accounts[1], 100000);
-      token.mint(accounts[2], 50000);
-      token.mint(accounts[3], 50000);
+    .then(async token => {
+      await token.mint(accounts[0], 800000);
+      await token.mint(accounts[1], 100000);
+      await token.mint(accounts[2], 50000);
+      await token.mint(accounts[3], 50000);
       console.log('SimpleERC20 deployed to:', token.address);
     });
 }
 
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
